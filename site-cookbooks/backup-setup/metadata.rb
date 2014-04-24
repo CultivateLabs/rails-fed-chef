44418b4a4ef55979531f801230fd9041c39b7c00
Brian Leslie <bjl1984@gmail.com>
2014-04-24 14:56:02 -0500

    Initial stab at a backup cookbook based of the backup gem. Could use some more refinement and testing.

---
+++ site-cookbooks/backup-setup/metadata.rb
@@ -0,0 +1,9 @@
+name             'backup-setup'
+maintainer       'Brian Leslie'
+maintainer_email 'brian.leslie@inklingmarkets.com'
+license          'All rights reserved'
+description      'Installs/Configures backup-setup'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          "cron"
