2777aae1b303c77c96893fdaca3e3cdf82a40eaf
Brian Leslie <bjl1984@gmail.com>
2014-07-07 13:03:02 -0500

    Fixed bug where config files without any nginx settings fails

--- site-cookbooks/nginx-setup/recipes/default.rb
+++ site-cookbooks/nginx-setup/recipes/default.rb
@@ -11,7 +11,7 @@ include_recipe "nginx::source"
 node.default[:nginx] = Chef::Mixin::DeepMerge.merge(node[:nginx], [ node[:app_data][:nginx] ])
 
 template "#{node[:nginx][:dir]}/sites-available/#{node[:app_name]}" do
-  if node['app_data']['nginx']['ssl_on'] && !node['app_data']['nginx']['load_balancer_enabled']
+  if  node['app_data']['nginx'] && node['app_data']['nginx']['ssl_on'] && !node['app_data']['nginx']['load_balancer_enabled']
     source "ssl_site.conf.erb"
   else
     source "site.conf.erb"
