524b85397fa1d16e78e62597a453cd580aad7c9c
Brian Leslie <bjl1984@gmail.com>
2014-08-06 18:47:12 -0500

    Support CIDR for allowed_app_servers and rails_servers

--- site-cookbooks/memcached-setup/recipes/default.rb
+++ site-cookbooks/memcached-setup/recipes/default.rb
@@ -2,7 +2,11 @@ if node['app_data']['rails_servers']
   node['app_data']['rails_servers'].each do |server|
     firewall_rule "memcached" do
       port      11211
-      source    "#{server}/32"
+      if server.include? "/"
+        source    "#{server}"
+      else
+        source    "#{server}/32"
+      end
       action    :allow
     end
   end
